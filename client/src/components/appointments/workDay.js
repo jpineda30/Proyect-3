8c62d3c65f2cb62b8a7e71f94701b2ff5a3ce884
pepe30 <jose.pinedaesquer@gmail.com>
2021-03-10 20:34:47 -0600

    added mock for appointments front

---
+++ client/src/components/appointments/workDay.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Slot from "./hourSlot";
+
+
+function WorkDay(){
+    return(  
+<div>
+
+                    <div className="flex-col back2">
+                            <div className="top-green">21-03-2021</div>
+                            <div className="flex-col p-1">
+                                <Slot/>
+                                <Slot/>
+                                <Slot/>
+                                <Slot/>
+                            </div>
+                          </div>
+
+
+</div>
+);
+}
+
+export default WorkDay;
--- client/src/components/pages/appointments.js
+++ client/src/components/pages/appointments.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AddBtn from "../general/AddBtn";
 import AppointmentCRUD from "../appointments/appointmentCRUD";
+import WorkDay from "../appointments/workDay";
 
 function Appointments(){
 
@@ -8,16 +9,21 @@ function Appointments(){
     return(
 
         
-        <div className="content flex-col">
+        <div className="flex-col scroll">
         
-        <div className="flex-col flex-center">
-        <h1>Appointments</h1>
-        <AddBtn/>
-        <AppointmentCRUD/>
-    
-    </div>   
-    
-</div>    
+            <div className="flex-col flex-center">
+
+                <h1>Appointments</h1>
+                <AddBtn/>
+                <div className="flex-row flex-around fw">
+                    <WorkDay></WorkDay>
+                    <WorkDay></WorkDay>
+                    <WorkDay></WorkDay>
+                </div>   
+            
+            </div>   
+            
+        </div>    
 
 
 
