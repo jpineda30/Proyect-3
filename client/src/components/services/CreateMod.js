d084e83bd6786e74bee159e6a0e05b7ff8cee5bf
Ibmug <rosv58@gmail.com>
2021-03-25 21:53:04 -0600

    meh

--- client/src/components/services/CreateMod.js
+++ client/src/components/services/CreateMod.js
@@ -1,110 +1,96 @@
-import React, {useRef } from "react";
+import React, { useRef } from "react";
 import API from "../../utils/API";
 
-function CreateMod(props){
+function CreateMod(props) {
+	const serviceName = useRef();
+	const serviceCost = useRef();
+	const serviceDescription = useRef();
+	var visibility = "visible";
+	var edition = false;
 
-  const serviceName = useRef();
-  const serviceCost = useRef();
-  const serviceDescription = useRef();
-  var visibility = "visible"
-  var edition = false;
+	if (props.type == "view") {
+		visibility = "hidden";
+		edition = true;
 
-  if(props.type == "view")
-  {
-    visibility = "hidden"; edition=true
+		getById(props.ide);
+	}
 
-    getById(props.ide);
+	if (props.type == "edit") {
+		getById(props.ide);
+	}
 
-  }
+	const getById = (id) => {
+		//api call then populate fields
+	};
 
-  if(props.type == "edit")
-  {
-    
-    getById(props.ide);
+	const saveService = () => {
+		let service = {
+			name: serviceName.current.value,
+			price: parseInt(serviceCost.current.value),
+			details: serviceDescription.current.value,
+		};
 
-  }
+		if (service.name != "" && service.cost != "") {
+			console.log(service);
 
+			if (props.type == "create") {
+				API.createService(service).then((service) => {
+					props.addService(service.data);
+				});
+				props.close();
+			} else if (props.type == "update") {
+				//update service
+			} else {
+				//view
+			}
+		} else {
+			alert("Empty inputs lazy ass");
+		}
+	};
 
-  const getById = (id)=>{
+	return (
+		<div className="flex-col flex-center modal-child">
+			<h1>Service {props.type}</h1>
+			<div className="flex-col ">
+				<label className="">Service Name</label>
+				<input
+					type="text"
+					ref={serviceName}
+					placeholder="Service name"
+					disabled={edition}
+				/>
+			</div>
 
-    //api call then populate fields
+			<div className="flex-col my-1 ">
+				<label className="">Service Cost</label>
+				<input
+					type="number"
+					ref={serviceCost}
+					placeholder="$1"
+					disabled={edition}
+				/>
+			</div>
 
-    
+			<div className="flex-col ">
+				<label className="">Service Description</label>
+				<input
+					className="height50"
+					type="text"
+					ref={serviceDescription}
+					disabled={edition}
+				/>
+			</div>
 
-  };
- 
-
-  const saveService = ()=>{
-
-    
-    let service = {
-      name:serviceName.current.value,
-      price:parseInt(serviceCost.current.value),
-      details:serviceDescription.current.value
-    }
-
-    if(service.name != "" && service.cost != "")
-    {
-      console.log(service);
-
-      if(props.type == "create")
-      {
-        API.createService(service).then(service=>{
-          props.addService(service.data);
-        })
-        props.close();
-      }
-      else if (props.type == "update") 
-      {
-        //update service
-      }
-      else{
-        //view
-        
-      }
-    }
-    else{
-      alert("Empty inputs lazy ass");
-    }
-
-    
-
-  }
-
-    return(
-      
-      
-        
-      <div className="flex-col flex-center modal-child">
-        <h1>Service {props.type}</h1>
-        <div className="flex-col ">
-          <label className="">Service Name</label>
-          <input type="text" ref={serviceName} placeholder="Service name" disabled={edition}/>
-
-        </div>
-
-        <div className="flex-col my-1 ">
-          <label className="">Service Cost</label>
-          <input type="number" ref={serviceCost} placeholder="$1" disabled={edition}/>
-
-        </div>
-
-        <div className="flex-col ">
-          <label className="">Service Description</label>
-          <input className="height50" type="text" ref={serviceDescription} disabled={edition}/>
-
-        </div>
-        
-        <div className="flex-row flex-center">
-
-        <div className={visibility + " button-small"} onClick={saveService}>Save</div>
-        <div className="button-small" onClick={props.close}>Cancel</div> 
-
-        </div>
-    </div>
-     
-
-    );
+			<div className="flex-row flex-center">
+				<div className={visibility + " button-small"} onClick={saveService}>
+					Save
+				</div>
+				<div className="button-small" onClick={props.close}>
+					Cancel
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default CreateMod;
