2d723df1e512ccf71b88cfcb17a04015f2d65219
Alheli Miranda <alhealhe72@hotmail.com>
2021-03-27 14:52:39 -0400

    changing the name file

--- routes/api/service.js
+++ routes/api/service.js
@@ -1,44 +1,34 @@
 var db = require("../../models");
 var passport = require("../../config/passport.js");
-const {Service} = require("../../controllers/index");
+const { Service } = require("../../controllers/index");
 //const ScheduleC = require("../../controllers")
 //console.log(ScheduleC);
 const router = require("express").Router();
 
-router.route("/")
-.get(
-    function(req, res) {
-        console.log("checking");
-    if (!req.user) {
+router.route("/").get(function (req, res) {
+  console.log("checking");
+  if (!req.user) {
+    // The user is not logged in, send back an empty object
+    res.status(404).json({});
+  } else {
+    console.log("logged");
+    //Findall
+    Service.findServiceById({})
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  }
+});
 
-      // The user is not logged in, send back an empty object
-      res.status(404).json({});
-    } else {      
-      console.log("logged");
-      //Findall
-     Service.findServiceById({})      
-       .then(dbModel => res.json(dbModel))
-      .catch(err=> res.status(422).json(err));
-    }
-  });
+router.route("/update").post(Service.update);
 
-  router.route("/update")
-  .post(
-        Service.update
-  )
-
-<<<<<<< HEAD
- router.route("/create")
-  .post(
-      //Podriamos revisar si el usuario esta loggeado para crear el servicio.
-      Service.create
-  )
-=======
- /* router.route("/create")
+router.route("/create").post(
+  //Podriamos revisar si el usuario esta loggeado para crear el servicio.
+  Service.create
+);
+/* router.route("/create")
   .post(
       //Podriamos revisar si el usuario esta loggeado para crear el servicio.
       Service.create
   ).then(dbRes => res.json(dbRes))*/
->>>>>>> master
 
-  module.exports = router;
+module.exports = router;
