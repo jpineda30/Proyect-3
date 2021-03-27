b3a2c2257a9369fafb420abaf0c5b184f10181c9
Ibmug <rosv58@gmail.com>
2021-03-27 13:53:54 -0600

    Mas cambios..

--- client/src/components/general/crudActions.js
+++ client/src/components/general/crudActions.js
@@ -1,60 +1,25 @@
 import React from "react";
 
-<<<<<<< HEAD
-
-function CrudActions(props){
-
-   
-
-    function onDelete(){
-        
-        props.toggle("delete",props.ide);
-    }
-
-    function onEdit(){
-        
-        props.toggle("edit",props);
-    }
-
-    function onView(){
-        
-        props.toggle("view",props);
-    }
-
-    return(
-
-        
-        <div className="flex-row flex-around">
-        <i onClick={onView} className="fas fa-eye fa-2x"></i>
-        <i onClick={onEdit} className="far fa-edit fa-2x"></i>
-        <i onClick={onDelete} className="fas fa-trash-alt fa-2x"></i>
-    </div>   
-
-
-
-    );
-=======
 function CrudActions(props) {
-  function onDelete() {
-    props.toggle("delete", props.service._id);
-  }
+	function onDelete() {
+		props.toggle("delete", props.service._id);
+	}
 
-  function onEdit() {
-    props.toggle("edit", props.service);
-  }
+	function onEdit() {
+		props.toggle("edit", props.service);
+	}
 
-  function onView() {
-    props.toggle("view", props.service);
-  }
+	function onView() {
+		props.toggle("view", props.service);
+	}
 
-  return (
-    <div className="flex-row flex-around">
-      <i onClick={onView} className="fas fa-eye fa-2x"></i>
-      <i onClick={onEdit} className="far fa-edit fa-2x"></i>
-      <i onClick={onDelete} className="fas fa-trash-alt fa-2x"></i>
-    </div>
-  );
->>>>>>> master
+	return (
+		<div className="flex-row flex-around">
+			<i onClick={onView} className="fas fa-eye fa-2x"></i>
+			<i onClick={onEdit} className="far fa-edit fa-2x"></i>
+			<i onClick={onDelete} className="fas fa-trash-alt fa-2x"></i>
+		</div>
+	);
 }
 
 export default CrudActions;
