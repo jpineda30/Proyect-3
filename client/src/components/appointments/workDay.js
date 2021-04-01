fbfb655dc2b7373219bfc26c6a03c24c68bfcc33
pepe30 <jose.pinedaesquer@gmail.com>
2021-03-31 20:31:57 -0600

    added patient list to appointments creation

--- client/src/components/appointments/workDay.js
+++ client/src/components/appointments/workDay.js
@@ -1,22 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Slot from "./hourSlot";
 import AddBtn2 from "../general/AddBtn2";
+import API from "../../utils/API-";
 
 function WorkDay(props) {
-  //get props.date and find in data base
-  //get appointments
-  //if doesnt exist create date in bd
+  const [appointments, setAppointments] = useState();
+
+  useEffect(() => {
+    //get props.date and find in data base
+    /* API.getAppByID().then((result) => {
+      setAppointments(result);
+    });*/
+  }, []);
+
   return (
     <div>
       <div className="flex-col back2 work-day">
         <div className="top-green flex-row flex-acenter flex-between">
           <p className="mini"></p>
           <p>{props.date ? props.date : "00/00/0000"}</p>
-          <AddBtn2 open={props.open} />
+          <AddBtn2 open={props.open} date={props.date} />
         </div>
-        <div className="flex-col p-1">
-          <Slot open={props.open} close={props.close} />
-          <Slot open={props.open} close={props.close} />
+        <div className="flex-col p-1 dates-scroll">
+          <Slot
+            open={props.open}
+            close={props.close}
+            patient={"Marcela Dupeyron"}
+            service={"Head amputation"}
+            start={"10:00 AM"}
+            end={"11:00 AM"}
+          />
         </div>
       </div>
     </div>
--- client/src/components/general/AddBtn2.js
+++ client/src/components/general/AddBtn2.js
@@ -1,26 +1,19 @@
 import React from "react";
 
-function AddBtn2(props){
+function AddBtn2(props) {
+  function onCreate() {
+    props.open("create", props.date);
+  }
 
-    function onCreate(){
-        
-        props.open("create");
-    }
-
-    return(
-
-        
-        <div className="button-mini flex-row flex-around flex-center" onClick={onCreate} data-name={props.name}>
-           
-           
-                <i className="fas fa-plus fa-2x"></i>
-               
-            
-        </div>    
-
-
-
-    );
+  return (
+    <div
+      className="button-mini flex-row flex-around flex-center"
+      onClick={onCreate}
+      data-name={props.name}
+    >
+      <i className="fas fa-plus fa-2x"></i>
+    </div>
+  );
 }
 
 export default AddBtn2;
