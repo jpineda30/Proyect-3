4725ecc9d679fcf7f1a3d7c514fc7b45b58fcac6
Ibmug <rosv58@gmail.com>
2021-04-03 12:45:14 -0600

    Adding name and service instead of id.

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -1,52 +1,59 @@
 const { Schedule } = require("../models");
 
 module.exports = {
-  findAppointment: function (q) {
-    //console.log(q);
-    //console.log(res);
-    // console.log(req.params);
-    return Schedule.find({ day: q });
-    // // .sort({date})
-    //  .then(dbModel => res.json(dbModel))
-    //  .catch(err=> res.status(422).json(err));
-  },
-  findAllAppointment: function (q) {
-    //	console.log(q);
-    //console.log(res);
-    // console.log(req.params);
-    return Schedule.find({});
-    // // .sort({date})
-    //  .then(dbModel => res.json(dbModel))
-    //  .catch(err=> res.status(422).json(err));
-  },
-  findAppointmentByDay: function (q) {
-    // return Schedule.find(q);
+	findAppointment: function (q) {
+		//console.log(q);
+		//console.log(res);
+		// console.log(req.params);
+		return Schedule.find({ day: q });
+		// // .sort({date})
+		//  .then(dbModel => res.json(dbModel))
+		//  .catch(err=> res.status(422).json(err));
+	},
+	findAllAppointment: function (q) {
+		//	console.log(q);
+		//console.log(res);
+		// console.log(req.params);
+		return Schedule.find({});
+		// // .sort({date})
+		//  .then(dbModel => res.json(dbModel))
+		//  .catch(err=> res.status(422).json(err));
+	},
+	findAppointmentByDay: function (q) {
+		// return Schedule.find(q);
 
-    return Schedule.find({ day: q });
-    // 	.then((dbModel) => res.json(dbModel))
-    // 	.catch((err) => res.status(422).json(err));
-  },
+		// Schedule.find({ day: q })
+		// 	.populate("service")
+		// 	.then((appointment) => {
+		// 		console.log(appointment);
+		// 		return appointment;
+		// 	}); // key to populate
 
-  create: function (req, res) {
-    console.log(req.body);
-    let { start: startT, end: endT, day, patient, service } = req.body;
+		return Schedule.find({ day: q }).populate("service").populate("patient");
+		// 	.then((dbModel) => res.json(dbModel))
+		// 	.catch((err) => res.status(422).json(err));
+	},
 
-    Schedule.create({
-      startT,
-      endT,
-      day,
-      patient,
-      service,
-      //Here goes the schedule obj
-      /*       day: props.day,
+	create: function (req, res) {
+		console.log(req.body);
+		let { start: startT, end: endT, day, patient, service } = req.body;
+
+		Schedule.create({
+			startT,
+			endT,
+			day,
+			patient,
+			service,
+			//Here goes the schedule obj
+			/*       day: props.day,
       patient: patient,
       service: service,
       start: startTime.current.value,
       end: endTime.current.value, */
-    })
-      .then((dbModel) => res.json(dbModel))
-      .catch((err) => {
-        res.status(422).json(err);
-      });
-  },
+		})
+			.then((dbModel) => res.json(dbModel))
+			.catch((err) => {
+				res.status(422).json(err);
+			});
+	},
 };
