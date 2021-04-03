6e5ba10c35ef1576582573cff8bdc472116c1c1f
Alheli Miranda <alhealhe72@hotmail.com>
2021-04-02 22:20:18 -0400

    merging

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -1,52 +1,52 @@
 const { Schedule } = require("../models");
 
 module.exports = {
-	findAppointment: function (q) {
-		//console.log(q);
-		//console.log(res);
-		// console.log(req.params);
-		return Schedule.find({ day: q });
-		// // .sort({date})
-		//  .then(dbModel => res.json(dbModel))
-		//  .catch(err=> res.status(422).json(err));
-	},
-	findAllAppointment: function (q) {
-		//	console.log(q);
-		//console.log(res);
-		// console.log(req.params);
-		return Schedule.find({});
-		// // .sort({date})
-		//  .then(dbModel => res.json(dbModel))
-		//  .catch(err=> res.status(422).json(err));
-	},
-	findAppointmentByDay: function (q) {
-		// return Schedule.find(q);
-		//console.log(q);
-		return Schedule.find(q);
-		// 	.then((dbModel) => res.json(dbModel))
-		// 	.catch((err) => res.status(422).json(err));
-	},
+  findAppointment: function (q) {
+    //console.log(q);
+    //console.log(res);
+    // console.log(req.params);
+    return Schedule.find({ day: q });
+    // // .sort({date})
+    //  .then(dbModel => res.json(dbModel))
+    //  .catch(err=> res.status(422).json(err));
+  },
+  findAllAppointment: function (q) {
+    //	console.log(q);
+    //console.log(res);
+    // console.log(req.params);
+    return Schedule.find({});
+    // // .sort({date})
+    //  .then(dbModel => res.json(dbModel))
+    //  .catch(err=> res.status(422).json(err));
+  },
+  findAppointmentByDay: function (q) {
+    // return Schedule.find(q);
+    //console.log(q);
+    return Schedule.find(q);
+    // 	.then((dbModel) => res.json(dbModel))
+    // 	.catch((err) => res.status(422).json(err));
+  },
 
-	create: function (req, res) {
-		console.log(req.body);
-		let { start: startT, end: endT, day, patient, service } = req.body;
+  create: function (req, res) {
+    console.log(req.body);
+    let { start: startT, end: endT, day, patient, service } = req.body;
 
-		Schedule.create({
-			startT,
-			endT,
-			day,
-			patient,
-			service,
-			//Here goes the schedule obj
-			/*       day: props.day,
+    Schedule.create({
+      startT,
+      endT,
+      day,
+      patient,
+      service,
+      //Here goes the schedule obj
+      /*       day: props.day,
       patient: patient,
       service: service,
       start: startTime.current.value,
       end: endTime.current.value, */
-		})
-			.then((dbModel) => res.json(dbModel))
-			.catch((err) => {
-				res.status(422).json(err);
-			});
-	},
+    })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => {
+        res.status(422).json(err);
+      });
+  },
 };
