31dabfdcb7ebe874287e7b65bb8223463dd95949
pepe30 <jose.pinedaesquer@gmail.com>
2021-04-02 20:08:29 -0600

    working get appointment by date

--- client/src/components/pages/appointments.js
+++ client/src/components/pages/appointments.js
@@ -6,115 +6,135 @@ import MyModal from "../general/Modal";
 import CreateMod from "../appointments/CreateMod";
 import DeleteMod from "../general/DeleteMod";
 import API from "../../utils/API-";
+import moment from "moment";
 
 class Appointments extends Component {
-	constructor() {
-		super();
-		this.state = {
-			crudActive: false,
-			action: <CreateMod close={this.closeModal}></CreateMod>,
-			date: new Date(),
-		};
-	}
+  constructor() {
+    super();
+    this.state = {
+      crudActive: false,
+      action: <CreateMod close={this.closeModal}></CreateMod>,
+      date: new Date(),
+      slots: [],
+    };
+  }
 
-	convertDate(date) {
-		var yyyy = date.getFullYear().toString();
-		var mm = (date.getMonth() + 1).toString();
-		var dd = date.getDate().toString();
+  convertDate(date) {
+    var yyyy = date.getFullYear().toString();
+    var mm = (date.getMonth() + 1).toString();
+    var dd = date.getDate().toString();
 
-		var mmChars = mm.split("");
-		var ddChars = dd.split("");
+    var mmChars = mm.split("");
+    var ddChars = dd.split("");
 
-		return (
-			yyyy +
-			"-" +
-			(mmChars[1] ? mm : "0" + mmChars[0]) +
-			"-" +
-			(ddChars[1] ? dd : "0" + ddChars[0])
-		);
-	}
+    return (
+      yyyy +
+      "-" +
+      (mmChars[1] ? mm : "0" + mmChars[0]) +
+      "-" +
+      (ddChars[1] ? dd : "0" + ddChars[0])
+    );
+  }
 
-	onChange = (date) => {
-		this.setState({
-			date,
-		});
-	};
+  onChange = async (date) => {
+    await this.setState({
+      date,
+    });
 
-	closeModal = () => {
-		this.setState({
-			crudActive: false,
-		});
-	};
+    this.loadDates();
+  };
 
-	openModal = (param, day) => {
-		switch (param) {
-			case "edit":
-				this.setState({
-					crudActive: true,
-					action: (
-						<CreateMod close={this.closeModal} name={"edition"}></CreateMod>
-					),
-				});
-				break;
-			case "create":
-				this.setState({
-					crudActive: true,
-					action: (
-						<CreateMod
-							close={this.closeModal}
-							name="creation"
-							day={day}
-						></CreateMod>
-					),
-				});
-				break;
-			case "delete":
-				this.setState({
-					crudActive: true,
-					action: (
-						<DeleteMod close={this.closeModal} name={"appointment"}></DeleteMod>
-					),
-				});
-				break;
-			default:
-				this.setState({
-					crudActive: true,
-					action: <CreateMod close={this.closeModal}></CreateMod>,
-				});
-				break;
-		}
-	};
+  closeModal = () => {
+    this.setState({
+      crudActive: false,
+    });
+  };
 
-	render() {
-		return (
-			<>
-				<Menu />
-				<div className="flex-col scroll">
-					<div className="flex-col flex-center">
-						<h1>Appointments</h1>
+  openModal = (param, day) => {
+    switch (param) {
+      case "edit":
+        this.setState({
+          crudActive: true,
+          action: (
+            <CreateMod close={this.closeModal} name={"edition"}></CreateMod>
+          ),
+        });
+        break;
+      case "create":
+        this.setState({
+          crudActive: true,
+          action: (
+            <CreateMod
+              close={this.closeModal}
+              name="creation"
+              day={day}
+            ></CreateMod>
+          ),
+        });
+        break;
+      case "delete":
+        this.setState({
+          crudActive: true,
+          action: (
+            <DeleteMod close={this.closeModal} name={"appointment"}></DeleteMod>
+          ),
+        });
+        break;
+      default:
+        this.setState({
+          crudActive: true,
+          action: <CreateMod close={this.closeModal}></CreateMod>,
+        });
+        break;
+    }
+  };
 
-						<div className="flex-row flex-wrap flex-jcenter">
-							<WorkDay
-								open={this.openModal}
-								close={this.closeModal}
-								date={this.state.date.toDateString()}
-								test={this.onChange}
-							></WorkDay>
-							<div className="calendar p-1 flex-col">
-								{" "}
-								<Calendar onChange={this.onChange} value={this.state.date} />
-							</div>
-							<MyModal
-								status={this.state.crudActive}
-								toggle={this.closeModal}
-								type={this.state.action}
-							/>
-						</div>
-					</div>
-				</div>
-			</>
-		);
-	}
+  loadDates = () => {
+    let formatCalendarDate = moment.utc(this.state.date).format("L");
+
+    let pack = { day: formatCalendarDate };
+    API.getAppointmentsByDate(pack)
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          slots: res.data,
+        });
+      })
+      .catch((err) => {
+        console.log("bazinga!");
+      });
+  };
+
+  render() {
+    return (
+      <>
+        <Menu />
+        <div className="flex-col scroll">
+          <div className="flex-col flex-center">
+            <h1>Appointments</h1>
+
+            <div className="flex-row flex-wrap flex-jcenter">
+              <WorkDay
+                open={this.openModal}
+                close={this.closeModal}
+                date={this.state.date.toDateString()}
+                slots={this.state.slots}
+              ></WorkDay>
+              <div className="calendar p-1 flex-col">
+                {" "}
+                <Calendar onChange={this.onChange} value={this.state.date} />
+              </div>
+              <MyModal
+                status={this.state.crudActive}
+                toggle={this.closeModal}
+                type={this.state.action}
+              />
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Appointments;
--- routes/api/appointment.js
+++ routes/api/appointment.js
@@ -6,29 +6,30 @@ const { Schedule } = require("../../controllers/index");
 const router = require("express").Router();
 
 router.route("/day").post(function (req, res) {
-	//console.log(req.body);
-	if (!req.user) {
-		res.status(404).json({ message: "Not Logged in" });
-	} else {
-		//console.log(req.body);
-		Schedule.findAppointmentByDay({ day: req.body.day })
-			.then((dbModel) => res.json(dbModel))
-			.catch((err) => res.status(422).json(err));
-	}
-	// });
+  console.log("Testing this bitch", req.body.day);
+
+  if (!req.user) {
+    res.status(404).json({ message: "Not Logged in" });
+  } else {
+    //console.log(req.body);
+    Schedule.findAppointmentByDay(req.body.day)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  }
+  // });
 });
 
 router.route("/month").get(function (req, res) {
-	if (!req.user) {
-		res.status(404).json({});
-	}
+  if (!req.user) {
+    res.status(404).json({});
+  }
 });
 
 router.route("/all").get(Schedule.findAllAppointment);
 
 router.route("/create").post(
-	//Podriamos revisar si el usuario esta loggeado para crear el servicio.
-	Schedule.create
+  //Podriamos revisar si el usuario esta loggeado para crear el servicio.
+  Schedule.create
 );
 
 module.exports = router;
